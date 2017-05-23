6a9e181697666dda9ac800e9da398e28b7e59236
Butch Marshall <butch.a.marshall@gmail.com>
2017-05-22 21:16:56 -0400

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Mapbox from 'react-native-mapbox-gl';
+Mapbox.setAccessToken('TOKEN_HERE');
+import { MapView } from 'react-native-mapbox-gl';
+
+export default class ReactNativeMapboxGlExample extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+        <MapView
+              style={{width: 300, height: 300}}
+              ref={map => { this._map = map; }}
+              styleURL={"mapbox://styles/mapbox/dark-v9"} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativeMapboxGlExample', () => ReactNativeMapboxGlExample);
